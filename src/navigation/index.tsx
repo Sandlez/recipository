edd8d386a540453d6409f5bb1ae04931e131399f
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-01-29 13:25:24 +0000

    Refactoring the info modal redirect to it's own component

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -16,6 +16,7 @@ import ModalScreen from '../screens/ModalScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
+import InfoRedirect from '../components/InfoRedirect';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 
@@ -69,18 +70,7 @@ function BottomTabNavigator() {
           title: 'Tab One',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
-            <Pressable
-              onPress={() => navigation.navigate('Modal')}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}>
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15 }}
-              />
-            </Pressable>
+	    <InfoRedirect navigation={navigation} />
           ),
         })}
       />
